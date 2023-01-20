494dab84b95577eb02c1b44d10dcb703614968ac
Luis Enrique <Luishao_90@hotmail.com>
2023-01-19 18:22:22 -0600

    Subiendo archivos primarios

---
+++ script.js
@@ -0,0 +1,29 @@
+function encriptar(){
+    var palabra=document.getElementById("mensaje").value;
+
+    var textoEncriptado =palabra.replace(/e/img,"enter");
+    var textoEncriptado =textoEncriptado.replace(/i/img,"imes");
+    var textoEncriptado =textoEncriptado.replace(/a/img,"ai");
+    var textoEncriptado =textoEncriptado.replace(/o/img,"ober");
+    var textoEncriptado =textoEncriptado.replace(/u/img,"ufat");
+
+    
+    document.getElementById("texto-encriptado").innerHTML=textoEncriptado;
+}
+function desEncriptar(){
+    var palabra=document.getElementById("texto-encriptado").value;
+
+    var textoEncriptado =palabra.replace(/enter/img,"e");
+    var textoEncriptado =textoEncriptado.replace(/imes/img,"i");
+    var textoEncriptado =textoEncriptado.replace(/ai/img,"a");
+    var textoEncriptado =textoEncriptado.replace(/ober/img,"o");
+    var textoEncriptado =textoEncriptado.replace(/ufat/img,"u");
+
+    document.getElementById("texto-encriptado").innerHTML=textoEncriptado;
+}
+
+function copiarTexto(){
+    var contenido =document.querySelector("#texto-encriptado");
+    contenido.select();
+    document.execCommand("copy");
+}
